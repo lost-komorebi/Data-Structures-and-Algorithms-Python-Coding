33f150cbdb37164ec075eb28d48b3f4c0799cfed
komorebi <caoxin19921027@gmail.com>
2023-02-07 16:27:47 +0700

    use deque to implement palindrome checker

---
+++ stack_and_queue/exercise/palindrome_checker.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'komorebi'
+
+from stack_and_queue.deque import Deque
+import sys
+sys.path.append('..')
+
+
+def palindrome_checker(string):
+    d = Deque()
+    for i in string:
+        d.add_rear(i)
+    for i in range(d.size() // 2):
+        front = d.remove_front()
+        rear = d.remove_rear()
+        if front != rear:
+            return False
+    return True
+
+
+if __name__ == '__main__':
+    print(palindrome_checker('abcdba'))
+    print(palindrome_checker('madam'))
