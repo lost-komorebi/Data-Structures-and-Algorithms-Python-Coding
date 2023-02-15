5b7a1329568ebb5aa19f9e1ca4d86ab61d0d66f6
komorebi <caoxin19921027@gmail.com>
2023-02-15 22:06:54 +0700

    add thickness parameter

--- recursion/fractal_tree.py
+++ recursion/fractal_tree.py
@@ -8,16 +8,17 @@
 
 from turtle import *
 
-def tree(length_of_branch, t):
+
+def tree(length_of_branch, thickness, t):
     if length_of_branch > 5:
+        t.width(thickness)
         t.forward(length_of_branch)
         t.right(20)
-        tree(length_of_branch-15, t)
+        tree(length_of_branch-15, thickness-3, t)
         t.left(40)
-        tree(length_of_branch-10, t)
+        tree(length_of_branch-10, thickness-2, t)
         t.right(20)
         t.backward(length_of_branch)
-
 
 
 t = Turtle()
@@ -27,5 +28,5 @@ t.up()
 t.backward(300)
 t.down()
 t.color('green')
-tree(110, t)
+tree(110, 22, t)
 my_win.exitonclick()
