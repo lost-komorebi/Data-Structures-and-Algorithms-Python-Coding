a8a23ce022540aac492b1200f4ec8e9ea9766f48
komorebi <caoxin19921027@gmail.com>
2023-02-20 13:28:11 +0700

    add short bubble sort

--- sort/bubble_sort.py
+++ sort/bubble_sort.py
@@ -16,7 +16,26 @@ def bubble_sort(li: list):
                 li[j], li[j + 1] = li[j + 1], li[j]  # swap location
 
 
+def short_bubble_sort(li: list):
+    """ 
+    we loop through the list len(li) - 1 times, 
+    if during a loop, we couldn't find any elements to swap
+    it means the list is ordered, so we stop the loop
+    """
+    for pass_num in range(len(li) - 1, 0, -1):
+        need_swap = False  # assume li is ordered
+        for i in range(pass_num):
+            if li[i] > li[i+1]:
+                need_swap = True  # if we found any unordered part, we still need to swap
+                li[i], li[i+1] = li[i+1], li[i]
+        if need_swap == False:  # no swap need to do, current li is ordered
+            break
+
+
 if __name__ == '__main__':
     my_list = [9, 1, 6, 3, 4, 7, 2, 8, 0, 5]
     bubble_sort(my_list)
     print(my_list)
+    my_list1 = [44, 20, 17, 26, 31, 54, 55, 77, 93]
+    short_bubble_sort(my_list1)
+    print(my_list1)
