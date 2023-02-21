6739aea1e653514efe34a3b76f1c6d4a378ac7c2
komorebi <caoxin19921027@gmail.com>
2023-02-21 09:05:51 +0700

    reduce loop times

--- sort/selection_sort.py
+++ sort/selection_sort.py
@@ -11,13 +11,16 @@ space complexity: O(1)
 
 
 def selection_sort(li: list):
-    for i in range(len(li)):
-        min_index = i
-        for j in range(
-                i, len(li)):  # find the minimum value from the unsorted area
+    # for n numbers, we just need to loop n - 1 times, we can get the list ordered
+    for i in range(len(li) - 1):
+        min_index = i  # we assume the first number in the unsorted area is the smallest
+        # find the minimum value from the unsorted area
+        # we don't need to compare i vs i, so we start from i + 1
+        for j in range(i+1, len(li)):
             if li[min_index] > li[j]:
                 min_index = j
         # put minimum value at the sorted area
+        # index < i belong to sorted area
         li[i], li[min_index] = li[min_index], li[i]
 
 
