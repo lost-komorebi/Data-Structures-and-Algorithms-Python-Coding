111643d47afd6f41773d01183a7805edd678d0ff
komorebi <caoxin19921027@gmail.com>
2023-02-22 18:15:03 +0700

    add another implementation

--- sort/quick_sort.py
+++ sort/quick_sort.py
@@ -24,8 +24,48 @@ def quick_sort(li: list):
     return quick_sort(left) + [pivot] + quick_sort(right)
 
 
+def quick_sort1(li, left=None, right=None):
+    if left is None:
+        left = 0
+    if right is None:
+        right = len(li) - 1
+    if left < right:  # if left smaller right, it means sublist contains at least two elements
+        pivot_pos = partition(li, left, right)
+        quick_sort1(li, left, pivot_pos-1)
+        quick_sort1(li, pivot_pos+1, right)
+
+
+def partition(li, left, right):
+    """ calculate index of new pivot """
+    l = left  # left_mark index
+    r = right - 1  # right_mark index is the second last element
+    pivot = li[right]  # set last element as pivot
+
+    while l < r:
+        # left mark move to right until find element greater than pivot
+        while li[l] < pivot and l < right:
+            l += 1
+        # right mark move to left until find element smaller than pivot
+        while li[r] >= pivot and r > left:
+            r -= 1
+        if l < r:
+            li[l], li[r] = li[r], li[l]
+
+    # right_mark moves to the left side of left_mark, i.e: r < l
+    # swap element at left mark and pivot
+    # l will be the index of new pivot
+    if li[l] > pivot:
+        li[l], li[right] = li[right], li[l]
+
+    return l
+
+
 if __name__ == '__main__':
-    my_list = [random.randint(0, 100) for _ in range(10)]
+
+    my_list = [9, 1, 6, 3, 4, 7, 2, 8, 0, 5]
     print(my_list)
     my_list = quick_sort(my_list)
+    print(my_list)
+    my_list = [9, 1, 6, 3, 4, 7, 2, 8, 0, 5]
+    quick_sort1(my_list)
     print(my_list)
