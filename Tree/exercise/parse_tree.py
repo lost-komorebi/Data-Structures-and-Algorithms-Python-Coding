b444e6ecdb5e8014d605ea111212d3383f0eb6e2
komorebi <caoxin19921027@gmail.com>
2023-03-10 13:57:22 +0800

    handle boolean statements (and, or, and not)

--- Tree/exercise/parse_tree.py
+++ Tree/exercise/parse_tree.py
@@ -13,13 +13,18 @@ rules:
 and descend to the left child.
 2. If the current token is a number, 
 set the root value of the current node to the number and return to the parent.
-3. If the current token is in the list ['+','−','/','*'], 
+3. If the current token is in the list ['+','−','/','*','and','or'], 
 set the root value of the current node to the operator represented by the current token. 
 Add a new node as the right child of the current node and descend to the right child.
 4. If the current token is a ')', go to the parent of the current node.
+5. If the current token is a ')', go to the parent of the current node. Then do same operations
+with rule 3
 """
 
 
+
+
+import operator
 class Stack:
     def __init__(self):
         self.stack_list = []
@@ -88,57 +93,98 @@ def build_parse_tree(expression):
         elif is_number(i):  # rule 2
             current_tree.set_root_val(i)
             current_tree = exp_stack.pop()
-        elif i in '+-*/':  # rule 3
+        elif i in ['+', '-', '*', '/', 'and', 'or']:  # rule 3
             current_tree.set_root_val(i)
             current_tree.insert_right('')
             exp_stack.push(current_tree)
             current_tree = current_tree.get_right_child()
-        elif i == ')':  # rule 4
+        elif i == ')':  # rule 4 go to the parent of current node
             current_tree = exp_stack.pop()
+        elif i in ['not']:  # rule 5
+            current_tree = exp_stack.pop()
+            current_tree.set_root_val(i)
+            current_tree.insert_right('right')
+            exp_stack.push(current_tree)
+            current_tree = current_tree.get_right_child()
         else:
             raise ValueError
     return exp_tree
 
 
 def exp_split(expression):
-    exp = expression.replace(' ', '')
+    """ split support expresions with space and with no space """
+    exp = expression.replace(' ', '').lower()
     result = []
     number = ''  # a variable to store number
+    boolean = ''  # a variable to store boolean statements
     for i in exp:
-        if i not in '()+-*/':
+        if i not in ['(', ')', '+', '-', '*', '/'] and not i.isalpha():
             number += i
+            if boolean != '':
+                result.append(boolean)
+            boolean = ''  # reset boolean
+        elif i.isalpha():
+            boolean += i
+            if number != '' and is_number(number):
+                result.append(float(number))
+            number = ''  # reset number
         else:
-            # when we meet '()+-*/', then we add current number to result
+            # when we meet ['+', '-', '*', '/', 'and', 'or', 'not'], then we add current number to result
             # and set number = ''
-            if number != '':
-                result.append(number)
-            number = ''
+            if boolean != '':
+                result.append(boolean)
+            boolean = ''  # reset boolean
+            if number != '' and is_number(number):
+                result.append(float(number))
+            number = ''  # reset number
             result.append(i)
-    print(result)
     return result
 
 
 def is_number(i):
-    """ check if a string is number """
-    if i.isdigit():
+    """ check if a string is number(int, float, negetive number) """
+    try:
+        float(i)
         return True
-    if i.replace('.', '').replace('-', '').isdigit():
-        return True
-    return False
+    except ValueError:
+        return False
 
 
 def evaluate(tree: BinaryTree):
     """ evaluate the parse tree """
+    operators = {'+': operator.add, '-': operator.sub, '*': operator.mul,
+                 '/': operator.truediv, 'and': fn_and, 'or': fn_or, 'not': fn_not}
     left = tree.get_left_child()
     right = tree.get_right_child()
-    if left and right:  # if a node has left and right, so this node must be a operator
-        fn = tree.get_root_val()
-        return str(eval(evaluate(left) + fn + evaluate(right)))
-    else:  # leaf noede
-        return tree.get_root_val()
+    if tree.get_root_val() == 'not':
+        fn = operators[tree.get_root_val()]
+        return fn(right)
+    else:
+        if left and right:  # if a node has left and right, so this node must be a operator
+            fn = operators[tree.get_root_val()]
+            return fn(evaluate(left), evaluate(right))
+        else:  # leaf noede
+            return (tree.get_root_val())
+
+
+def fn_and(a, b):
+    return a and b
+
+
+def fn_or(a, b):
+    return a or b
+
+
+def fn_not(a):
+    return not a
 
 
 if __name__ == '__main__':
-    exp = '( 31 + ( ( 4 * 5.2 ) / 2 ) )'
-    print(build_parse_tree(exp).post_order_traversal())
-    print(evaluate(build_parse_tree(exp)))
+    exp1 = '( 31 + ( ( 4 * 5.2 ) / 2 ) )'
+    exp_tree1 = build_parse_tree(exp1)
+    # print(exp_tree1.post_order_traversal())
+    print(evaluate(exp_tree1))
+    exp2 = '((not 1) and (1 and 2) or (4 and 4) and (not 5))'
+    exp_tree2 = build_parse_tree(exp2)
+    # print(exp_tree2.post_order_traversal())
+    print(evaluate(exp_tree2))
