50aa58c2559ccc025d2b136f1ffc946551a1684f
komorebi <caoxin19921027@gmail.com>
2023-03-18 18:20:30 +0800

    finish first common ancestor

--- algorithm/exercise/find_common_ancestor.py
+++ algorithm/exercise/find_common_ancestor.py
@@ -8,6 +8,10 @@ Design an algorithm and write code to find the first common ancestor of two node
 Avoid storing additional nodes in a data structure.
 NOTE: This is not necessarily a binary search tree.
 """
+"""
+solution:
+only when n1, n2 locate in different sides of root, then the root is the first common ancestor of n1 and n2
+"""
 
 
 class Node:
@@ -15,50 +19,52 @@ class Node:
         self.value = value
         self.right = right
         self.left = left
-        self.parent = None
 
 
-def insert(root, value):
+def find_node_in_tree(root, node):
+    """ if a node exists in a tree, return True; else, return False """
     if not root:
-        return Node(value)
-    if value < root.value:
-        root.left = insert(root.left, value)
-    else:
-        root.right = insert(root.right, value)
-    return root
+        return False
+    if root == node:
+        return True
+    else:  # recursively find in left subtree or right subtree
+        return (find_node_in_tree(root.left, node)) or (find_node_in_tree(root.right, node))
 
 
-def search(root, value):
-    if not root:
-        return
-    if value < root.value:
-        return search(root.left, value)
-    elif value > root.value:
-        return search(root.right, value)
-    else:
+def find_common_ancestor(n1, n2, root):
+    n1_in_tree = find_node_in_tree(root.left, n1)
+    n2_in_tree = find_node_in_tree(root.left, n2)
+    # when n1 and n2 locate in different side of a node,the node is first common ancestor
+    if n1_in_tree ^ n2_in_tree:  # operator ^ means only one is True then it will return True
         return root
-
-
-def findFirstCommonAncestor(n1, n2, root):
-    node_n1 = search(root, n1)
-    node_n2 = search(root, n2)
-    print(node_n1.parent.value, node_n2.parent.value)
-
-
-def in_order_traversal(root: Node):
-    if root.left:
-        in_order_traversal(root.left)
-    print(root.value)
-    if root.right:
-        in_order_traversal(root.right)
+    else:  # find first comman ancestor recursively
+        if n1_in_tree:
+            return find_common_ancestor(n1, n2, root.left)
+        else:
+            return find_common_ancestor(n1, n2, root.right)
 
 
 if __name__ == '__main__':
-    my_tree = Node(100)
-    my_list = [90, 110, 89, 91, 109, 111, 87, 93, 92, 94, 86, 88]
-    for i in my_list:
-        insert(my_tree, i)
-    #in_order_traversal(my_tree)
-    # for i in my_list:
-    #     assert search(my_tree, i).value == i
-    findFirstCommonAncestor(88, 93, my_tree)
+    my_tree = Node(55)
+    node_44 = Node(44)
+    node_77 = Node(77)
+    node_22 = Node(22)
+    node_99 = Node(99)
+    node_35 = Node(35)
+    node_88 = Node(88)
+    node_90 = Node(90)
+    node_95 = Node(95)
+    node_54 = Node(54)
+    node_33 = Node(33)
+    my_tree.left = node_44
+    my_tree.right = node_77
+    node_44.left = node_22
+    node_44.right = node_99
+    node_22.left = node_35
+    node_22.right = node_88
+    node_88.left = node_54
+    node_99.left = node_90
+    node_99.right = node_95
+    node_90.right = node_33
+
+    print(find_common_ancestor(node_54, node_88, my_tree))
