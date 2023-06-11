a0a610226c604faf60e7a5a9869096318c66765b
komorebi <caoxin19921027@gmail.com>
2023-06-11 19:41:18 +0800

    create bst with minimum height

--- algorithm/exercise/minimal_tree.py
+++ algorithm/exercise/minimal_tree.py
@@ -5,8 +5,12 @@ __author__ = 'komorebi'
 """
 Given a sorted (increasing order) array with unique integer elements,
 write an algorithm to create a binary search tree with minimal height.
+To build a binary search tree with minimal height
+we can assume the sorted array inorder traversal of a tree
+as we know, the order of inorder traversal is left, root, right
+so we can choose the middle element as root
+then use same approach on it's left and right side recursively
 """
-
 
 class BSTNode:
     def __init__(self, data=None, left=None, right=None):
@@ -14,60 +18,19 @@ class BSTNode:
         self.left = left
         self.right = right
 
-
-class BST:
-    def __init__(self):
-        self.root = None
-
-    def insert(self, k):
-        if not self.root:
-            self.root = BSTNode(k)
-        else:
-            cur = self.root
-            if k < cur.data:
-                if cur.left:
-                    while cur.left:
-                        if k < cur.left.data:
-                            cur = cur.left
-                        else:
-                            cur.right = BSTNode(k)
-                    cur.left = BSTNode(k)
-                else:
-                    cur.left = BSTNode(k)
-            else:
-                if cur.right:
-                    while cur.right:
-                        if k > cur.right.data:
-                            cur = cur.right
-                        else:
-                            cur.left = BSTNode(k)
-                    cur.right = BSTNode(k)
-                else:
-                    cur.right = BSTNode(k)
-
-    def level_order_traversal(self):
-        if not self.root:
-            return
-        q = [self.root]
-        visited = []
-        while q:
-            cur = q.pop(0)
-            if cur.left:
-                q.append(cur.left)
-            if cur.right:
-                q.append(cur.right)
-            visited.append(cur.data)
-        print(visited)
+def minimal_tree(li: list) -> BSTNode:
+    length = len(li)
+    if length == 0:
+        return 
+    mid = length // 2
+    root = BSTNode(li[mid])
+    root.left = minimal_tree(li[0: mid])
+    root.right = minimal_tree(li[mid + 1:])
+    return root
 
 
-def minimal_tree(sorted_array):
-    pass
 
 
 if __name__ == '__main__':
-    #sorted_array = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-    sorted_array = [5,4,2,3]
-    my_bst = BST()
-    for i in sorted_array:
-        my_bst.insert(i)
-    my_bst.level_order_traversal()
+    sorted_array = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+    my_tree = minimal_tree(sorted_array)
